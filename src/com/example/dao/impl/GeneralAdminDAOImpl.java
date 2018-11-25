113c61be3f7fc1cc68017e5261aefae62014b08f
陈文权 <@DESKTOP-9LIRV3E>
2018-11-25 21:42:09 +0800

    25

--- src/com/example/dao/impl/GeneralAdminDAOImpl.java
+++ src/com/example/dao/impl/GeneralAdminDAOImpl.java
@@ -9,69 +9,76 @@ import com.example.pojo.entity.Admin;
 import com.example.pojo.entity.User;
  
 public class GeneralAdminDAOImpl implements GeneralAdminDAO {
-	private HibernateTemplate hTemplate;
+	private HibernateTemplate hibernateTemplat;
 	
-	public void setHibernateTemplate(HibernateTemplate hTemplate) {
-		this.hTemplate = hTemplate;
+	public void setHibernateTemplat(HibernateTemplate hibernateTemplat) {
+		this.hibernateTemplat = hibernateTemplat;
 	}
 	
-    /**
-     * @author 陈文权
-     * 普通管理员通过账号进行登录
-     */
+	/**
+	 * 普通管理员通过账号进行登录
+	 * @author 陈文权
+	 * @param admin 从service传递过来的含name的admin参数
+	 * @return 返回admin对象或null
+	 */
 	@Override
 	public Admin findByAdminName(Admin admin) {
 		String hql="from Admin where adminName=? and adminPassword=? and adminJurisdiction=?";
-		List<Admin> list=(List<Admin>) this.hTemplate.find(hql, admin.getAdminName(),admin.getAdminPassword(),true);
+		List<Admin> list=(List<Admin>) this.hibernateTemplat.find(hql, admin.getAdminName(),admin.getAdminPassword(),true);
 		if(list.size()>0)    
 			return list.get(0);
 		else
 		return null;
 	}
-
 	/**
-	 * 普通管理员通过邮箱账号进行登录
+	 * 普通管理员通过邮箱进行登录
+	 * @param admin 从service传递过来的含email的admin参数
+	 * @return 返回admin对象或null
 	 */
 	@Override
 	public Admin findByEmail(Admin admin) {
 		String hql="from Admin where adminEmail=? and adminPassword=? and adminJurisdiction=?";
-		List<Admin> list=(List<Admin>) this.hTemplate.find(hql, admin.getAdminEmail(),admin.getAdminPassword(),true);
+		List<Admin> list=(List<Admin>) this.hibernateTemplat.find(hql, admin.getAdminEmail(),admin.getAdminPassword(),true);
 		if(list.size()>0)
 			return list.get(0);
 		else
 		return null;
 	}
-
 	/**
 	 * 普通管理员通过手机号进行登录
+	 * @param admin 从service传递过来的含phone的admin参数
+	 * @return 返回admin对象或null
 	 */
 	@Override
 	public Admin findByPhone(Admin admin) {
 		String hql="from Admin where adminPhone=? and adminPassword=? and adminJurisdiction=?";
-		List<Admin> list=(List<Admin>) this.hTemplate.find(hql, admin.getAdminPhone(),admin.getAdminPassword(),true);
+		List<Admin> list=(List<Admin>) this.hibernateTemplat.find(hql, admin.getAdminPhone(),admin.getAdminPassword(),true);
 		if(list.size()>0)
 			return list.get(0);
 		else
 		return null;
 	}
-	
 	/**
-	 * 查看所有用户
+	 * 普通管理员查看所有用户
+	 * @return 返回User对象List集合
 	 */
 	@Override
 	public List<User> findAllUser() {
 		   String hql="from User";
-		   List<User> allUser=(List<User>) this.hTemplate.find(hql);
+		   List<User> allUser=(List<User>) this.hibernateTemplat.find(hql);
 		   return allUser;
 	}
-
-	/**
-     * 更新密码
+    /**
+     * 普通管理员更改密码
+     * @param admin 从service传递过来的admin参数
+	 * @param rePassword 从service传递过来的新密码的参数
      */
 	@Override
 	public void updatePassword(Admin admin,String rePassword) {
-           Admin a=this.hTemplate.get(Admin.class, admin.getAdminId());
+           Admin a=this.hibernateTemplat.get(Admin.class, admin.getAdminId());
            a.setAdminPassword(rePassword);
-           this.hTemplate.update(a);
+           this.hibernateTemplat.update(a);
 	}
+
+
 }
